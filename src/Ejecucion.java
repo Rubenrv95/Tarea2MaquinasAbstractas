456ea19b91d96e7b21a7bdf75690cb1334f6c302
Javiera Méndez <jaavi.mendez8@gmail.com>
2020-12-18 08:27:29 -0300

    Calcular

--- src/Ejecucion.java
+++ src/Ejecucion.java
@@ -9,18 +9,117 @@ public class Ejecucion {
     {
         this.variables = new HashMap<String, BigInteger>();
         this.scanner = new Scanner(System.in);
-        //ejecutar("read $n;");
         variables.put("$n", BigInteger.valueOf(1));
-        //ejecutar("write $n + 1;");
+        //variables.put("$fib1", BigInteger.valueOf(1));
+        //variables.put("$fib2", BigInteger.valueOf(1));
+
+        //ejecutar("read $n;");
+
         //ejecutar("write $n + 1;");
 
         ejecutar("$n = $fib1 + $fib2;");
+
+        //System.out.println(infixToPostfix("( 1000 + (354 * ( 2 * 355 ) ) + 1"));
+        //System.out.println(infixToPostfix("( 1 + 3 * ( 2 * 3 ) ) + 1"));
     }
 
 
-    public int calcular (String s) {
+    public BigInteger calcular (String operacion)
+    {
+        System.out.println(operacion);
+        // primero hay que reemplazar todas las $ por su valor, si no se encuentra.. ERROR
+        String[] terminos = operacion.split(" ");
+        BigInteger rFinal = null;
+        for(int i = 0; i < terminos.length; i++){
+            String t = terminos[i];
+            if(terminos[i].startsWith("$")){
+                BigInteger r = obtenerValor(terminos[i]);
+                if(r != null){
+                    terminos[i] = String.valueOf(r);
+                    System.out.println("r: "+r);
+                }
+                else{
+                    break;
+                }
+            }
+        }
 
-        return 0;
+        String op2 = "";
+        for(int i = 0; i < terminos.length; i++){
+
+            op2 = op2+" "+terminos[i];
+        }
+        System.out.println("Fin: "+op2);
+
+        //String o = infixToPostfix("(1000 + (354 * ( 2 * 355 ) ) + 1");
+        String o = infixToPostfix(op2);
+        //operacion = opPrueba.replace(" ", "");
+        System.out.println(o);
+
+        Stack<Integer> stack = new Stack<>();
+
+        int i;
+
+        for(i=0; i < o.length(); i++)
+        {
+            char c = o.charAt(i);
+
+            if(c == ' ')
+            {
+
+            }
+            else if(Character.isDigit(c))
+            {
+                int n = 0;
+
+                while(Character.isDigit(c))
+                {
+                    n = n*10 + (int)(c-'0');
+                    i++;
+                    c = o.charAt(i);
+                }
+                i--;
+                stack.push(n);
+            }
+            else
+            {
+                if(stack.size()>1){
+                    int val1 = stack.pop();
+                    int val2 = stack.pop();
+
+                    switch(c)
+                    {
+                        case '+':
+                            stack.push(val2+val1);
+                            break;
+
+                        case '-':
+                            stack.push(val2- val1);
+                            break;
+
+                        case '/':
+                            stack.push(val2/val1);
+                            break;
+
+                        case '*':
+                            stack.push(val2*val1);
+                            break;
+                    }
+                }
+            }
+        }
+        //System.out.println("R:" +stack.pop());
+
+        return BigInteger.valueOf(stack.pop());
+    }
+
+    public BigInteger obtenerValor(String key){
+        BigInteger r = null;
+        if (variables.containsKey(key)) {
+            return variables.get(key);
+        }
+        System.out.println("ERROR: la variable "+key+" no esta inicializada");
+        return null;
     }
 
     public void ejecutar(String linea){
@@ -57,25 +156,36 @@ public class Ejecucion {
                 scanner.next();
             }
         }
+        if(variables.containsKey(str[1]))
+        {
+            System.out.println("true");
+            variables.replace(str[1], n);
+        }
+        else{
+            variables.put(str[1], n);
+        }
 
+        /*
         while (it.hasNext())
         {
             Map.Entry pair = (Map.Entry) it.next();
-            //Si encuentro la key, por el de la operac
+
             if(pair.getKey().equals(str[1]))
             {
+                System.out.println("Antes: "+pair.getKey() + " = " + pair.getValue());
                 pair.setValue(n);
+                System.out.println("Dps: "+pair.getKey() + " = " + pair.getValue());
             }
-            System.out.println("Reemplazo: "+pair.getKey() + " = " + pair.getValue());
-        }
+        }*/
+        /*
         if(loEncontro == false){
             //Aqui se le agrega como value el resultado del calcular
             variables.put(str[1], n);
             System.out.println("put NUEVO: "+str[1]+" "+n);
-        }
+        }*/
     }
 
-    public void write(String sublinea)
+    public int write(String sublinea)
     {
         String str = sublinea.substring(6, sublinea.length()-1); //le quito el write y el ;
         System.out.println(str);
@@ -109,17 +219,25 @@ public class Ejecucion {
                         operacion = valor +" "+ operacion;
                         System.out.println(operacion);
                         loEncontro = true;
+                        BigInteger resultado = calcular(operacion);
+                        System.out.println("Resultado: "+operacion);
+                        System.out.println("Antes: "+pair.getKey() + " = " + pair.getValue());
+                        variables.replace(var, resultado);
+                        System.out.println("Despues: "+pair.getKey() + " = " + pair.getValue());
                         //Calcular y reemplazar
                     }
                 }
                 if(!loEncontro){
                     System.out.println("ERROR: La variable "+var+" no se encuentra inicializada");
+                    System.exit(0);
+                    return 0;
                 }
             }
         }
+        return 0;
     }
 
-    public void init(String linea)
+    public int init(String linea)
     {
         System.out.println(linea);
         String var = null;
@@ -138,29 +256,35 @@ public class Ejecucion {
             if(esValido)
             {
                 Iterator it = variables.entrySet().iterator();
-                boolean loEncontro = false;
 
                 BigInteger valor;
-
+                if(!variables.containsKey(var)){
+                    variables.put(var, BigInteger.valueOf(1));
+                    System.out.println("ERROR: La variable "+var+" no se encuentra inicializada");
+                    System.exit(0);
+                    return 0;
+                }
                 while (it.hasNext())
                 {
                     Map.Entry pair = (Map.Entry) it.next();
-                    //Si encuentro la key, por el de la operac
+
                     if(pair.getKey().equals(var))
                     {
                         valor = (BigInteger) (pair.getValue());
-                        String operacion = linea.substring(i+1, linea.length());
-                        operacion = valor +" "+ operacion;
+                        String operacion = linea.substring(i+1, linea.length()-1);
+
                         System.out.println("op:"+ operacion);
-                        loEncontro = true;
+                        BigInteger resultado = calcular(operacion);
+                        System.out.println("Resultado: "+resultado);
+                        System.out.println("Antes: "+pair.getKey() + " = " + pair.getValue());
+                        variables.replace(var, resultado);
+                        System.out.println("Despues: "+pair.getKey() + " = " + pair.getValue());
                         //Calcular y reemplazar
                     }
                 }
-                if(!loEncontro){
-                    System.out.println("ERROR: La variable "+var+" no se encuentra inicializada");
-                }
             }
         }
+        return 0;
     }
 
 
@@ -197,5 +321,61 @@ public class Ejecucion {
         }
 
         return s;
+    }
+
+    public String infixToPostfix(String expression) {
+        Stack<Character> stack = new Stack<Character>();
+        String postfixString = "";
+
+        for (int index = 0; index < expression.length(); ++index) {
+            char value = expression.charAt(index);
+            if (value == '(') {
+                stack.push('('); // Code Added
+            } else if (value == ')') {
+                Character oper = stack.peek();
+
+                while (!(oper.equals('(')) && !(stack.isEmpty())) {
+                    stack.pop();
+                    postfixString += oper.charValue();
+                    if (!stack.isEmpty()) // Code Added
+                        oper = stack.peek(); // Code Added
+                }
+                stack.pop(); // Code Added
+            } else if (value == '+' || value == '-') {
+                if (stack.isEmpty()) {
+                    stack.push(value);
+                } else {
+                    Character oper = stack.peek();
+                    while (!(stack.isEmpty() || oper.equals(('(')) || oper.equals((')')))) {
+                        oper = stack.pop(); // Code Updated
+                        postfixString += oper.charValue();
+                    }
+                    stack.push(value);
+                }
+            } else if (value == '*' || value == '/') {
+                if (stack.isEmpty()) {
+                    stack.push(value);
+                } else {
+                    Character oper = stack.peek();
+                    // while condition updated
+                    while (!oper.equals(('(')) && !oper.equals(('+')) && !oper.equals(('-')) && !stack.isEmpty()) {
+                        oper = stack.pop(); // Code Updated
+                        postfixString += oper.charValue();
+                    }
+                    stack.push(value);
+                }
+            } else {
+                postfixString += value;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            Character oper = stack.peek();
+            if (!oper.equals(('('))) {
+                stack.pop();
+                postfixString += oper.charValue();
+            }
+        }
+        return postfixString;
     }
 }
