67d8e37e85632cdb9c54bfca4cc802254fbda961
Matias Pizarro <pinkystylee98@gmail.com>
2020-12-20 17:48:22 -0300

    if y while listo (creo)

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
 
         Parser p = new Parser(pila);
         p.parseCodigo();
+        p.ejecutar();
         // String op = "1 + $pinky * $z";
         // //p.parseOperacion(op);
         //p.parseCondicion("$d1 + ( 3 * 8 + ( 5 - 2 ) ) != $d2");
