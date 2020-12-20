c2a0520a5d6ba92c48709bacd2249eba5417666f
Rubén Ramírez <rubenrv95@hotmail.com>
2020-12-20 20:38:49 -0300

    Chao infix
    :c

--- src/Ejecucion.java
+++ src/Ejecucion.java
@@ -251,62 +251,6 @@ public class Ejecucion {
         }
     }
 
-    public String infixToPostfix(String expression) {
-        Stack<Character> stack = new Stack<Character>();
-        String postfixString = "";
-
-        for (int index = 0; index < expression.length(); ++index) {
-            char value = expression.charAt(index);
-            if (value == '(') {
-                stack.push('('); // Code Added
-            } else if (value == ')') {
-                Character oper = stack.peek();
-
-                while (!(oper.equals('(')) && !(stack.isEmpty())) {
-                    stack.pop();
-                    postfixString += oper.charValue();
-                    if (!stack.isEmpty()) // Code Added
-                        oper = stack.peek(); // Code Added
-                }
-                stack.pop(); // Code Added
-            } else if (value == '+' || value == '-') {
-                if (stack.isEmpty()) {
-                    stack.push(value);
-                } else {
-                    Character oper = stack.peek();
-                    while (!(stack.isEmpty() || oper.equals(('(')) || oper.equals((')')))) {
-                        oper = stack.pop(); // Code Updated
-                        postfixString += oper.charValue();
-                    }
-                    stack.push(value);
-                }
-            } else if (value == '*' || value == '/') {
-                if (stack.isEmpty()) {
-                    stack.push(value);
-                } else {
-                    Character oper = stack.peek();
-                    // while condition updated
-                    while (!oper.equals(('(')) && !oper.equals(('+')) && !oper.equals(('-')) && !stack.isEmpty()) {
-                        oper = stack.pop(); // Code Updated
-                        postfixString += oper.charValue();
-                    }
-                    stack.push(value);
-                }
-            } else {
-                postfixString += value;
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            Character oper = stack.peek();
-            if (!oper.equals(('('))) {
-                stack.pop();
-                postfixString += oper.charValue();
-            }
-        }
-        return postfixString;
-    }
-
     public boolean verificarCondicion(String s) {
 
 
